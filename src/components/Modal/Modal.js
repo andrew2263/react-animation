c0984227681202725d6cab38af61f4ae603c0440
andrew2263 <andrew2263@gmail.com>
2023-01-23 19:40:43 +0200

    css_transition

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Transition } from 'react-transition-group';
+import { CSSTransition } from 'react-transition-group';
 
 import './Modal.css';
 
@@ -18,29 +19,25 @@ const modal = (props) => {
   ];
   */
   return (
-    <Transition
+    <CSSTransition
       mountOnEnter
       unmountOnExit
       in={props.show}
       timeout={animationTiming}
+      classNames={{
+        enter: '',
+        enterActive: 'ModalOpen',
+        exit: '',
+        exitActive: 'ModalClosed'
+      }}
     >
-      { state => {
-        const cssClasses = [
-          'Modal',
-          state === 'entering' 
-          ? 'ModalOpen'
-          : state === 'exiting' ? 'ModalClosed' : null
-        ];
-        return (
-          <div className={ cssClasses.join(' ') }>
-            <h1>A Modal</h1>
-            <button className="Button" onClick={props.closed}>
-              Dismiss
-            </button>
-          </div>
-        );
-      } }
-    </Transition>
+      <div className="Modal">
+        <h1>A Modal</h1>
+        <button className="Button" onClick={props.closed}>
+          Dismiss
+        </button>
+      </div>
+    </CSSTransition>
   );
 };
 
